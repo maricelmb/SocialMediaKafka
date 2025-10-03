d999c18ecd5fe349bafb428e141586e1e3033c72
maricel <maricelmb@gmail.com>
2025-10-03 22:22:32 +1000

    add the controller for adding a comment

---
+++ src/SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/AddCommentController.cs
@@ -0,0 +1,62 @@
+﻿using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Post.Cmd.Api.Commands;
+using Post.Common.DTOs;
+
+namespace Post.Cmd.Api.Controllers
+{
+    [Route("api/v1/[controller]")] //https://localhost:7014/api/v1/AddComment/86D27DF0-179B-464C-89C9-04A56D3998E5
+    [ApiController]
+    public class AddCommentController : ControllerBase
+    {
+        private readonly ILogger<AddCommentController> _logger;
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public AddCommentController(ILogger<AddCommentController> logger, ICommandDispatcher commandDispatcher)
+        {
+            _logger = logger;
+            _commandDispatcher = commandDispatcher;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AddCommentAsync(Guid id, AddCommentCommand command)
+        {
+            try
+            {
+                command.Id = id;
+                await _commandDispatcher.SendAsync<AddCommentCommand>(command);
+
+                return Ok(new BaseResponse
+                {
+                    Message = "Add comment request completed successfully!"
+                });
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Client made a bad request!");
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+            catch (AggregateNotFoundException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Could not retrieve aggregate, client passed an incorrect post ID targetting the aggregate.");
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error while processing request to add a comment to a post!";
+                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+                {
+                    Message = SAFE_ERROR_MESSAGE
+                });
+            }
+        }
+    }
+}
---
+++ src/SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/LikePostController.cs
@@ -0,0 +1,63 @@
+﻿using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Post.Cmd.Api.Commands;
+using Post.Common.DTOs;
+
+namespace Post.Cmd.Api.Controllers
+{
+    [Route("api/v1/[controller]")] //https://localhost:7014/api/v1/LikePost/86D27DF0-179B-464C-89C9-04A56D3998E5
+    [ApiController]
+    public class LikePostController : ControllerBase
+    {
+        private readonly ILogger<LikePostController> _logger;
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public LikePostController(ILogger<LikePostController> logger, ICommandDispatcher commandDispatcher)
+        {
+            _logger = logger;
+            _commandDispatcher = commandDispatcher;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> LikePostAsync(Guid id)
+        {
+            try
+            {
+                
+                await _commandDispatcher.SendAsync<LikePostCommand>(new LikePostCommand { Id = id});
+
+                return Ok(new BaseResponse
+                {
+                    Message = "Like post request completed successfully!"
+                });
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Client made a bad request!");
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+            catch (AggregateNotFoundException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Could not retrieve aggregate, client passed an incorrect post ID targetting the aggregate.");
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error while processing request to like post!";
+                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+                {
+                    Message = SAFE_ERROR_MESSAGE
+                });
+            }
+        }
+    }
+}
